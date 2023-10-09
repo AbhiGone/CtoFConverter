da1ccb8ec72f8c591dbd29481c1e7968eaafde93
AbhiGone <giriabhinaya@gmail.com>
2023-10-09 09:31:03 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double tempC;
+        double tempF;
+        boolean enteredTemp = true;
+
+        do {
+            System.out.println("Please enter a temperature in degrees C to be converted to degrees F:");
+            if (scan.hasNextDouble()) {
+                tempC = scan.nextDouble();
+                tempF = (tempC * 9 / 5) + (32);
+
+                System.out.printf("%-10s %7.2f", "Degrees C:", tempC);
+                System.out.printf("\n%-10s %7.2f", "Degrees F:", tempF);
+                enteredTemp = false;
+            }else{
+                System.out.println("you have entered an invalid temperature value. Please try again.");
+                scan.nextLine();
+            }
+
+        } while (enteredTemp);
+
     }
 }
